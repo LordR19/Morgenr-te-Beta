12f7aa1ef25b20ffbcace5faeb47b644c62aff9f
LordR19 <ramun.wilder@proton.me>
2023-10-08 15:12:03 +0200

    Revert "Birbal Sahni DNA"
    This reverts commit d871e375cf23a407626df179049a9034c3dcedc7.

---
+++ Hotfix 1.8.5.c
@@ -0,0 +1,9 @@
+
+- Variables shouldn't be lost by Revolutions and Secessions as much as in the past. Hopefully.
+- Add Brazilian Portuguese Translation
+- Fixed the Art Secession events
+- NBY Education gives +1 Standard of Living
+- Fixed Paleontologist Location for places except North America
+- AI Great Powers will get Excavation Teams more aggressively
+- AI Great Powers will try to get Excavation Licenses more aggressively
+- AI Great Britain will try to get Theory of Evolution a bit more aggressively
